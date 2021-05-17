a603843ddfbf50cd880403512b1ac9f5daa13b9c
兰德儿 <fangshi44775@mail.tanzk.com>
2021-05-17 15:14:46 +0800

    refactor: 修改站点根路径

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,36 +2,36 @@ module.exports = {
   themeConfig: {
     nav: [
       {
-        text: 'vue',
-        link: '/',
+        text: "vue",
+        link: "/"
       },
       {
-        text: 'math',
-        link: '/math/',
+        text: "math",
+        link: "/math/"
       },
       {
-        text: 'js',
-        link: '/js/',
-      },
+        text: "js",
+        link: "/js/"
+      }
     ],
     sidebar: [
       {
-        title: '算法', // 必要的
-        path: '/math/',
+        title: "算法", // 必要的
+        path: "/math/"
       },
       {
-        title: 'js',
-        path: '/js/',
+        title: "js",
+        path: "/js/"
       },
       {
-        title: 'vue',
-        path: 'vue/vuepress',
-      },
+        title: "vue",
+        path: "vue/vuepress"
+      }
     ],
-    displayAllHeaders: true,
+    displayAllHeaders: true
   },
-  title: 'my-note', // 设置网站标题
-  dest: './dist', // 设置输出目录
-  base: '/my-note/', // 设置站点根路径
-  repo: 'https://github.com/fs821031547/newnote.git', // 添加 github 链接
+  title: "my-note", // 设置网站标题
+  dest: "./dist", // 设置输出目录
+  base: "/newnote/", // 设置站点根路径
+  repo: "https://github.com/fs821031547/newnote.git" // 添加 github 链接
 };
